167bf86a1a1dfd297062953d44cfffb47f903eb7
NGN-Limjihoon <lonelynight1026@gmail.com>
2022-08-23 23:08:15 +0900

    TIL[node.js] : middleware middleware process & middleware modules - morgan - cookieparser

--- ch6-express-server/app.js
+++ ch6-express-server/app.js
@@ -1,25 +1,64 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+// create app
 const app = express();
-
+// config app setting(port ...)
 app.set('port', process.env.PORT || 3000);
 
-app.use((req, res, next) => {
-    console.log('모든 요청에 실행');
+// use morgan module
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+app.get('/',(req,res,next)=>{
+    req.cookies;
+    req.signedCookies;
+    // 'Set-Cookie' : `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly;Path=\`,
+    req.cookies('name',encodeURIComponent(name),{
+        expires: new Date(),
+        httpOnly: true,
+        path:'/',
+    })
 })
 
+// middleware
+// Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application’s request-response cycle.
+app.use((req, res, next) => {
+    console.log('1번째 미들웨어 실행');
+		next();
+
+}, (req,res,next) => {
+    console.log('2번째 미들웨어 실행');
+	// throw new Error('에러');
+});
+
+// router
+// Routing refers to how an application’s endpoints (URIs) respond to client requests
 app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행');
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.');
+    console.log('GET / 요청에서만 실행' );
 })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('/filteringQuery=?', (req, res) => {
-    res.send('filter query');
+app.get('/category/Javascript', (req,res)=>{
+		res.send('hello Javascript');
+});
+
+app.get('/category/:name', (req,res)=>{
+		res.send('hello wildcard');
+});
+
+app.use((req,res,next)=>{
+    res.send('404지롱');
+});
+
+// 에러 미들웨어에는 next 매개변수를 꼭 넣어주어야 함.
+app.use((err,req,res,next)=>{
+    console.error(err);
+    res.status(600).send('에러났지롱 안 알려주지롱');
 })
 
 app.listen(3000, () => {
