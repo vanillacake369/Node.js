0fe1d6d649905d93d79218c9f547bd59e5048a0b
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-02 17:51:17 +0900

    user,comment 테이블 생성 이후 연관관계 지정

--- ch7-sequelize/models/user.js
+++ ch7-sequelize/models/user.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-module.exports = class User extends Sequelize.Model{
-    static int(sequelize){
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
         return super.init({
             name: {
                 type: Sequelize.STRING(20),
@@ -9,32 +9,35 @@ module.exports = class User extends Sequelize.Model{
                 unique: true,
             },
             age: {
-                type:Sequelize.INTEGER.UNSIGNED,
-                allowNull:false,
+                type: Sequelize.INTEGER.UNSIGNED,
+                allowNull: false,
             },
-            married:{
-                type:Sequelize.BOOLEAN,
-                allowNull:false,
+            married: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
             },
-            comment:{
-                type:Sequelize.TEXT,
-                allowNull:true,
+            comment: {
+                type: Sequelize.TEXT,
+                allowNull: true,
             },
-            created_at:{
-                type:Sequelize.DATE,
-                allowNull:false,
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
                 defaultValue: Sequelize.NOW,
-            }
-        },{
+            },
+        }, {
             sequelize,
-            timestamps:false,
-            underscored:false,
-            modelName:'User',
-            tableName:'users',
-            paranoid:false,
-            charset:'utf8',
-            collate:'utf9_general_ci'
+            timestamps: false,
+            underscored: false,
+            modelName: 'User',
+            tableName: 'users',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
         });
     }
-    static associate(db){}
+
+    static associate(db) {
+        db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+    }
 };
