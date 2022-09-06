ca4f6a8306eb3e376c136e0f4d35e91389965d88
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-06 21:01:41 +0900

    주인 없는 댓글 생성 후, 주인 연결 시켜주기

--- ch7-sequelize/routes/comments.js
+++ ch7-sequelize/routes/comments.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const { Comment } = require('../models');
+const { Comment, User } = require('../models');
 
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
     try {
+        /* #1 
+        req.body.id로 넘어온 id -> Comment.commenter컬럼 : id와 연관관계 되어있음
+        req.body.id로 넘어온 comment -> Comment.comment컬럼
+        */
         const comment = await Comment.create({
             commenter: req.body.id,
             comment: req.body.comment,
         });
+
+        /* #2
+        1. req.body.id로 user 찾은 후
+        2. req.body.comment에 대한 comment 생성 : id(주인)이 없는 댓글 생성
+        3. user.addComment로 해당 댓글 연동
+        
+        const user = await User.findOne({ where: { id: req.body.id } });
+        const comment = await Comment.create({
+            comment: req.body.comment,
+        });
+        */
+        await user.addComment(comment);
         console.log(comment);
         res.status(201).json(comment);
     } catch (err) {
