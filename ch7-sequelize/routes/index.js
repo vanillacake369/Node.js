3f5242d9d6585ec13324d8d52fd28181ca1763f6
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-18 14:39:25 +0900

    app.js수정

--- ch7-sequelize/routes/index.js
+++ ch7-sequelize/routes/index.js
@@ -7,6 +7,8 @@ router.get('/', async (req, res, next) => {
     try {
         const users = await User.findAll();
         res.render('sequelize', { users });
+        // './views/sequelize.html' 내에 user변수를 처리해줌!
+        // views 경로는 app.js에서 nunjucks.config로 이미 지정해놨음!!
     } catch (err) {
         console.error(err);
         next(err);
