8367113f617b8771467349e40bfb0367259bbe63
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-18 16:50:25 +0900

    /routes/page.js 라우터 에러 수정

--- ch9-nodebird/lecture/routes/page.js
+++ ch9-nodebird/lecture/routes/page.js
@@ -1,20 +1,21 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.use((req, res, next) => {
     res.locals.user = null;
-    res.locals.follerCount = 0;
+    res.locals.followerCount = 0;
     res.locals.followingCount = 0;
-    res.locals.follwerIdList = [];
+    res.locals.followerIdList = [];
     next();
-}); // [] is declaring an array. {} is declaring an object.
+});// [] is declaring an array. {} is declaring an object.
 
 router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내정보-NodeBird' });
+    res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
 router.get('/join', (req, res) => {
-    res.render('join', { title: '회원가입-NodeBird' });
+    res.render('join', { title: '회원가입 - NodeBird' });
 });
 
 router.get('/', (req, res, next) => {
