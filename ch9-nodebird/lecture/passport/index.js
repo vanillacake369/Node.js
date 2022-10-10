09f64060b4544d4013b5b212383eb01558f99618
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-10-10 20:17:50 +0900

    passport/index.js와 routes/page.js,user.js > 팔로잉,팔로워 구별기능 구현 완료

--- ch9-nodebird/lecture/passport/index.js
+++ ch9-nodebird/lecture/passport/index.js
@@ -1,19 +1,30 @@
 const passport = require('passport');
-const local = require('./localStrategy.js');
+const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
 const User = require('../models/user');
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-    });
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
+      .then(user => done(null, user)) // req.user, req.isAuthenticated() 반환
+      .catch(err => done(err));
+  });
 
-    passport.deserializeUser((id, done) => {
-        User.findOne({ where: { id } })
-            .then(user => done(null, user))
-            .catch(err => done(err));
-    });
-    local();
-    kakao();
+  local();
+  kakao();
 };
--- ch9-nodebird/lecture/routes/page.js
+++ ch9-nodebird/lecture/routes/page.js
@@ -1,33 +1,64 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User, Hashtag } = require('../models');
+
 const router = express.Router();
 
-// res.locals 변수를 통해 rendering page의 변수에 값 대입
 router.use((req, res, next) => {
-    res.locals.user = null;
-    res.locals.followerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followerIdList = [];
-    next();
-});// [] is declaring an array. {} is declaring an object.
-
-// 각 요청에 대한 렌더링 지정
-// title : 브라우저 탭에 표시되는 텍스트 지정
-router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내 정보 - NodeBird' });
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  next();
 });
 
-router.get('/join', (req, res) => {
-    res.render('join', { title: '회원가입 - NodeBird' });
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-    const twits = [];
-    res.render('main', {
-        title: 'NodeBird',
-        twits,
-        user: req.user,
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
     });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
-// Export express.Router()
+router.get('/hashtag', async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+
+    return res.render('main', {
+      title: `${query} | NodeBird`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
 module.exports = router;
